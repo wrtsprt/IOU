12c6ddcf6443fd6b8d6cfe5d7bb6501b76dcf0f3
Raphael Doehring <git@raphas.net>
2011-11-06 11:12:19 +0100

    basic devise setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create! email: 'r@bc.de', :password => 'password1', :password_confirmation => 'password1'
+User.create! email: 'a@bc.de', :password => 'password1', :password_confirmation => 'password1'
