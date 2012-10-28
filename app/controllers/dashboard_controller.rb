5fd98c9c216f4702faa09bcbc950caafa93e1039
Raphael Doehring <git@raphas.net>
2012-10-28 20:08:33 +0100

    added line showing sum of all credits / debts for dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,22 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @users = []
+    @sum_debt = 0
+    @sum_credit = 0
+    User.without(current_user).each do |user|
+      sum       = user.owes(current_user)
+      own_debt = current_user.owes(user)
+
+      unless sum == 0 && own_debt == 0
+        @users << [user, (sum - own_debt).abs, sum > own_debt ? :green : :red]
+        if sum > own_debt
+          @sum_credit += (sum - own_debt).abs
+        else
+          @sum_debt += (sum - own_debt).abs
+        end
+      end
+    end
   end
 
 end
