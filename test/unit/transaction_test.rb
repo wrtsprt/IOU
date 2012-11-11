16dc7bfd89f63850edfdb57b79dec6ba54d4b488
Raphael Doehring <git@raphas.net>
2012-11-11 20:12:10 +0100

    using explicit ids in test instead of probably ambiguous statements

--- test/unit/transaction_test.rb
+++ test/unit/transaction_test.rb
@@ -3,44 +3,44 @@ require 'test_helper'
 class TransactionTest < ActiveSupport::TestCase
 
   test "simple transaction with one participant" do
-    Transaction.create! :name => "transaction1", :creditor => User.first.id, :debtors => [User.last.id], :amount => 10
-    assert_equal 0,  User.first.owes(User.last)
-    assert_equal 10, User.last.owes(User.first).to_i
+    Transaction.create! :name => "transaction1", :creditor => User.find(1).id, :debtors => [User.find(4).id], :amount => 10
+    assert_equal 0,  User.find(1).owes(User.find(4))
+    assert_equal 10, User.find(4).owes(User.find(1)).to_i
 
-    Transaction.create! :name => "transaction2", :creditor => User.last.id, :debtors => [User.first.id], :amount => 5.5
-    assert_equal 5.5,  User.first.owes(User.last).to_f
-    assert_equal 10,    User.last.owes(User.first), User.last.owes(User.first).to_i
+    Transaction.create! :name => "transaction2", :creditor => User.find(4).id, :debtors => [User.find(1).id], :amount => 5.5
+    assert_equal 5.5,  User.find(1).owes(User.find(4)).to_f
+    assert_equal 10,    User.find(4).owes(User.find(1)), User.find(4).owes(User.find(1)).to_i
   end
 
   test "split bill" do
-    Transaction.create! :name => "transaction1", :creditor => User.first.id, :debtors => [2,3,4], :amount => 9
+    Transaction.create! :name => "transaction1", :creditor => 1, :debtors => [2,3,4], :amount => 9
 
-    assert_equal 3,  User.find(2).owes(User.first).to_f
-    assert_equal 3,  User.find(3).owes(User.first).to_f
-    assert_equal 3,  User.find(4).owes(User.first).to_f
+    assert_equal 3,  User.find(2).owes(User.find(1)).to_f
+    assert_equal 3,  User.find(3).owes(User.find(1)).to_f
+    assert_equal 3,  User.find(4).owes(User.find(1)).to_f
     assert_equal 0,  User.find(1).owes(User.find(2))
   end
 
   test "split bill with uneven result" do
-    Transaction.create! :name => "transaction1", :creditor => User.first.id, :debtors => [2,3,4], :amount => 8.1
+    Transaction.create! :name => "transaction1", :creditor => 1, :debtors => [2,3,4], :amount => 8.1
 
-    sum = User.find(2).owes(User.first) + User.find(3).owes(User.first) + User.find(4).owes(User.first)
+    sum = User.find(2).owes(User.find(1)) + User.find(3).owes(User.find(1)) + User.find(4).owes(User.find(1))
     assert_equal 8.1, sum
     assert_equal 0, User.find(1).owes(User.find(2))
   end
 
   test "split bill with uneven result 1" do
-    Transaction.create! :name => "transaction1", :creditor => User.first.id, :debtors => [2,3,4], :amount => 7.8
+    Transaction.create! :name => "transaction1", :creditor => 1, :debtors => [2,3,4], :amount => 7.8
 
-    sum = User.find(2).owes(User.first) + User.find(3).owes(User.first) + User.find(4).owes(User.first)
+    sum = User.find(2).owes(User.find(1)) + User.find(3).owes(User.find(1)) + User.find(4).owes(User.find(1))
     assert_equal 7.8, sum
     assert_equal 0, User.find(1).owes(User.find(2))
   end
 
   test "split bill with uneven result 2" do
-    Transaction.create! :name => "transaction1", :creditor => User.first.id, :debtors => [2,3,4], :amount => 7.9
+    Transaction.create! :name => "transaction1", :creditor => 1, :debtors => [2,3,4], :amount => 7.9
 
-    sum = User.find(2).owes(User.first) + User.find(3).owes(User.first) + User.find(4).owes(User.first)
+    sum = User.find(2).owes(User.find(1)) + User.find(3).owes(User.find(1)) + User.find(4).owes(User.find(1))
     assert_equal 7.9, sum
     assert_equal 0, User.find(1).owes(User.find(2))
   end
